09c14adc7d969bff17a57ac74a5ede8d980c349f
mnkhmtv <diminnakh2209@gmail.com>
2022-01-17 02:54:50 +0300

    02:54 NEW

--- ex.py
+++ ex.py
@@ -1,28 +1,36 @@
+from cgitb import html
 from bs4 import BeautifulSoup
 import requests
 
 
-URL = "https://ege.sdamgia.ru/test?theme=73&print=true"
-XPATH = "#body918442 > p"
-
+URL = "https://math-ege.sdamgia.ru/"
+#XPATH = "#body918442 > p"
 
 def get_html():
     response = requests.get(url=URL)
-    with open("filename.html", "w+") as file:
+    with open("page.html", "w+") as file:
         file.writelines(response.text)
 
 
 
-def parse():
-    with open("filename.html", "r") as file:
+def get_answer():
+    with open("page.html", "r") as file:
         html = file.read()
         soup = BeautifulSoup(html, "lxml")
-        all = soup.find_all("div", class_="prob_maindiv")
+        all = soup.find_all("div", class_="ConstructorForm-Row")
+        global task
         for task in all:
-            p = task.find("p")
+            
+            task = task.find('div', class_="Link Link_wrap Link_pseudo ConstructorForm-TopicName ConstructorForm-TopicName_type_default Link_pseudoBlack")
+            print(task)
 
-        print(p)
+task = str(get_answer)
 
+
+def parse(): 
+
+    with open('task.txt', 'w') as file:
+        file.writelines(task)
 
 if __name__ == "__main__":
     parse()
