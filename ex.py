44443d79da344de77ab277c68751e5623b8cb197
mnkhmtv <diminnakh2209@gmail.com>
2022-01-17 03:06:20 +0300

    Не выходит значение a

--- ex.py
+++ ex.py
@@ -18,19 +18,21 @@ def get_answer():
         html = file.read()
         soup = BeautifulSoup(html, "lxml")
         all = soup.find_all("div", class_="ConstructorForm-Row")
-        global task
         for task in all:
             
-            task = task.find('div', class_="Link Link_wrap Link_pseudo ConstructorForm-TopicName ConstructorForm-TopicName_type_default Link_pseudoBlack")
-            print(task)
+            a = task.find('div', class_="Link Link_wrap Link_pseudo ConstructorForm-TopicName ConstructorForm-TopicName_type_default Link_pseudoBlack")
+            
+            print(a)
+        print('1')
 
-task = str(get_answer)
+
+task = get_answer
 
 
-def parse(): 
+#def parse(): 
 
-    with open('task.txt', 'w') as file:
-        file.writelines(task)
+#    with open('task.html', 'w') as file:
+#        file.writelines(task.text)
 
 if __name__ == "__main__":
-    parse()
+    get_answer()
