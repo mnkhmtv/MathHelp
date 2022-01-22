7dff92735ef7493c6a2f62f32827051a3b17c97b
mnkhmtv <diminnakh2209@gmail.com>
2022-01-22 23:25:35 +0300

    new

--- theme_2.py
+++ theme_2.py
@@ -1,45 +1,82 @@
-from cgitb import html, text
-from posixpath import dirname
-import re
-from bs4 import BeautifulSoup
-import requests
+import csv
 import os
 
-URL = 'https://ege.sdamgia.ru/test?id=43128379&nt=True&pub=False&print=true'
+from cgitb import html, text
+from operator import delitem, index
+from posixpath import dirname
+from tkinter import Listbox
+from bs4 import BeautifulSoup
+import requests
 
-def get_html():
-    response = requests.get(url=URL)
-    with open("test.html", "w+") as file:
+
+def get_html(url: str, file_name: str):
+    """ Скачивает HTML по URL и сохраняет в file_name
+    """
+    response = requests.get(url = url)
+    with open(file_name, "w+") as file:
         file.writelines(response.text)
-        print('1')
 
-def get_answer():
-    with open("test.html", "r") as file:
+
+def get_answer(file_name: str, theme: str, index: int = 1)->int:
+    """ Сохраняет вопрос и ответ в папку темы
+        Возвращает индекс файла на котором закончил
+        """
+    with open(file_name, "r") as file:
         html = file.read()
         soup = BeautifulSoup(html, "lxml")
         all = soup.find_all("div", class_="prob_maindiv")
-        i = 1
+        
         for taskanswer in all:
             if not taskanswer:
                 print('Task is not found')
                 continue
 
-            a = taskanswer.find('p', class_ = 'left_margin')
-            c = taskanswer.find('div', class_ = 'answer')
-            d = a, c
-            #print(d)
-            
-            save_file(file_name = str(i) + '.html',content = d)
-            i = i + 1
-            
+            exersice = taskanswer.find('p', class_ = 'left_margin')
+            answer = taskanswer.find('div', class_ = 'answer')
+            together = exersice, answer
+            task_file_name: str = 'Темы/' + theme + '/' + 'Задача_' + str(index) + '.html'
+            save_file(file_name = task_file_name,content = together)
+            index = index + 1
+        return index
 
+def load_urls():
+    with open('urls.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        return list(reader)
 
 
 def save_file(file_name, content): 
-
     with open(file_name, 'w') as file:
-
         file.write(str(content)) 
 
+
+def main():
+    files: list = load_urls()
+    current_theme_index: int = None
+    previous_theme_name: str = None
+    for file in files:
+        temp_filename = "temp_001.html"
+        print('1.Скачиваем HTML по URL: {}'.format(file['url']))
+        get_html(url= file['url'], file_name= temp_filename)
+        print('HTML сохранен!')    
+        print('2. Обрабатываю файл: ' + temp_filename)
+        
+        if previous_theme_name == file['theme']:
+            current_theme_index = get_answer(
+                file_name= temp_filename, theme= file['theme'], index= current_theme_index + 1
+            )      
+        else:
+            current_theme_index = get_answer(
+                file_name= temp_filename, theme= file['theme']
+            )
+        previous_theme_name = file['theme']
+        
+        print(f'Файл обработан!  Добавлено {current_theme_index} задач')
+        os.remove(temp_filename)
+        print('*'*20)
+
+
 if __name__ == "__main__":
-    get_answer()
+    # get_answer()
+    # load_urls()
+    main()
